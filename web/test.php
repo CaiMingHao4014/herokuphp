2eb793d4683eb107954b3b9e050aebb6e33cf3ba
CaiMingHao4014 <20006707@myrp.edu.sg>
2022-07-08 16:35:35 +0800

    initial commit

--- web/test.php
+++ web/test.php
@@ -1,10 +1,12 @@
 <!doctype html>
   <html lang="en">
 <head>
-<title>Test Page</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="Description of the webpage">
+  <title>Test Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Description of the webpage">
 </head>
+
 <body>
     Hello World!
 </body>
+</html>
